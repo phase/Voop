daeb2589f048f8fa07da05c1a1687e04521a405b
Phase <ph4sesaber@gmail.com>
2014-12-12 22:09:45 -0800

    Parser with Block

---
+++ src/xyz/_5th/voop/Block.java
@@ -0,0 +1,40 @@
+/**
+ * This file may be open source, 
+ * but that does not mean you own it. 
+ * Contact me at https://github.com/Phasesaber .
+ */
+package xyz._5th.voop;
+
+import java.util.ArrayList;
+
+/**
+ * Project: Voop
+ * 
+ * File: Block.java
+ * 
+ * Represents a block of code, such as an if statement.
+ * 
+ * @author Phase(Jadon Fowler) on Dec 12, 2014
+ */
+public abstract class Block{
+
+	private Block superBlock;
+	private ArrayList<Block> subBlocks;
+	
+	public Block(Block superBlock){
+		this.superBlock = superBlock;
+		this.subBlocks = new ArrayList<Block>();
+	}
+	
+	public Block getSuperBlock(){
+		return superBlock;
+	}
+	
+	public void addBlock(Block b){
+		subBlocks.add(b);
+	}
+	
+	public abstract void run();
+	
+	
+}
---
+++ src/xyz/_5th/voop/parser/Parser.java
@@ -0,0 +1,34 @@
+/**
+ * This file may be open source, 
+ * but that does not mean you own it. 
+ * Contact me at https://github.com/Phasesaber .
+ */
+package xyz._5th.voop.parser;
+
+import xyz._5th.voop.Block;
+import xyz._5th.voop.token.Tokenizer;
+
+/**
+ * Project: Voop
+ * 
+ * File: Parser.java
+ * 
+ * @author Phase(Jadon Fowler) on Dec 12, 2014
+ */
+public abstract class Parser<T extends Block> {
+	
+	/**
+	 * Takes a line and checks to see if it should parse it.
+	 * @param line = line to parse
+	 * @return should it parse?
+	 */
+	public abstract boolean shouldParse(String line);
+	
+	/**
+	 * Take the super block and the tokenizer for the line and return a block of this parser's type.
+	 * @param superBlock = Block above this block
+	 * @param tokenizer = Tokenizer for line
+	 * @return
+	 */
+	public abstract T parse(Block superBlock, Tokenizer tokenizer);
+}
